5caee1bcff3a265dcd89697b7f628d3859804348
kdzhou <kezhen-zhou@uiowa.edu>
2023-07-19 11:39:48 -0500

    Added done and fail callbacks to redirect page

--- WorkSite/Scripts/worksite.project.js
+++ WorkSite/Scripts/worksite.project.js
@@ -6,9 +6,29 @@
         $(".add-project-container").on("click", ".add-project-btn", function () {
             var $form = $(".add-project-form");
             $form.attr("action", $(this).data("action"));
-            utilities.submitAjaxForm($form);
+            var pageUrl = $(this).data("redirect");
+            utilities.submitAjaxForm($form, createProjectsCallbacks(setPageUrl(pageUrl)));
             $(this).prop("disabled", true);
-        })
+        });
+    }
+
+    function setPageUrl(url) {
+        return function () {
+            history.pushState(null, null, url)
+        };
+    }
+
+    function createProjectsCallbacks(actions) {
+        return {
+            doneCallback: function () {
+                window.alert("Success");
+                window.location.href = "/Projects";
+            },
+            failCallback: function () {
+                window.alert("Failed");
+                window.location.href = "/Projects";
+            }
+        };
     }
 
     return {
